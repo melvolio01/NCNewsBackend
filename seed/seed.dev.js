c3ee49a317454268319cbf08379b4cc52c4ef881
andy hunt <andyhunt200@hotmail.com>
2018-08-17 09:54:57 +0100

    Updating db config info

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,7 +1,10 @@
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/northcoders_news';
+// let DB_URL = 'mongodb://localhost:27017/northcoders_news';
+const DB_URL = process.env.DB_URL || require('../config/db-config.js').dbConfig[process.env.NODE_ENV].DB_URL;
 const data  = require('./devData/index.js')
+
+// if (process.env === 'production') DB_URL = 'mongodb://andy:confpass1@ds123852.mlab.com:23852/northcoders_news_ah';
 
 console.log('Seed dev page running');
 mongoose.connect(DB_URL, { useNewUrlParser: true })
