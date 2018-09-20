2c989dd458e9bef0366703c90aecaec61e729e71
andy hunt <andyhunt200@hotmail.com>
2018-09-20 13:01:36 +0100

    Minor changes

--- controllers/articles.js
+++ controllers/articles.js
@@ -83,8 +83,6 @@ const addCommentToArticle = (req, res, next) => {
         belongs_to: commentArticle,
         created_by: newComment.created_by
     })
-        .populate("created_by")
-        .populate("belongs_to")
         .then(comment => {
             res.status(201).send({ comment });
         })
