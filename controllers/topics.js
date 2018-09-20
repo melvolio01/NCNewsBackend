9d8b4a3cbc852675815657cf7db421689d864034
andy hunt <andyhunt200@hotmail.com>
2018-09-20 12:52:18 +0100

    Minor changes to populate data

--- controllers/topics.js
+++ controllers/topics.js
@@ -3,25 +3,25 @@ const Article = require('../models/index.js').Article;
 
 const getAllTopics = (req, res, next) => {
     Topic.find()
-    .then(topics => {
-        res.status(200).send({ topics })
-    });
+        .then(topics => {
+            res.status(200).send({ topics })
+        });
 }
 
 const getArticlesByTopic = (req, res, next) => {
     let topic_slug = req.params;
-    Article.find({ 'belongs_to' : `${topic_slug.topic_slug}` })
-    .populate("created_by")
-    .then(articles => {
-        if (articles.length > 0) res.status(200).send({ articles })
-        else res.status(404).send({message: 'No articles for that topic'});
-    })
-    .catch(next);
+    Article.find({ 'belongs_to': `${topic_slug.topic_slug}` })
+        .populate("created_by")
+        .then(articles => {
+            if (articles.length > 0) res.status(200).send({ articles })
+            else res.status(404).send({ message: 'No articles for that topic' });
+        })
+        .catch(next);
 }
 
 const addArticleByTopic = (req, res, next) => {
     let newArticle = req.body;
-    
+
     let articleSlug = req.params.topic_slug;
     Article.create({
         belongs_to: articleSlug,
@@ -29,12 +29,12 @@ const addArticleByTopic = (req, res, next) => {
         body: newArticle.body,
         created_by: newArticle.username
     })
-    .then(article => {
-        res.status(201).send({article});
-    })
-    .catch(err => {
-        next({status:400, message: 'The article is incomplete, please check all required fields have been completed.'});
-    });
+        .then(article => {
+            res.status(201).send({ article });
+        })
+        .catch(err => {
+            next({ status: 400, message: 'The article is incomplete, please check all required fields have been completed.' });
+        });
 }
 
 
