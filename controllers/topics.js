b08f30b182cc21a83a6b38edab835042881448d1
andy hunt <andyhunt200@hotmail.com>
2018-09-21 09:49:15 +0100

    Comment count for topic routes

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,5 +1,6 @@
 const Topic = require('../models/index.js').Topic;
 const Article = require('../models/index.js').Article;
+const { addCommentCount } = require('../utils')
 
 const getAllTopics = (req, res, next) => {
     Topic.find()
@@ -12,6 +13,7 @@ const getArticlesByTopic = (req, res, next) => {
     let topic_slug = req.params;
     Article.find({ 'belongs_to': `${topic_slug.topic_slug}` })
         .populate("created_by")
+        .then(articles => addCommentCount(articles, Comment))
         .then(articles => {
             if (articles.length > 0) res.status(200).send({ articles })
             else res.status(404).send({ message: 'No articles for that topic' });
